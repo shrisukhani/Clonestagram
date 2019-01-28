32f856e2f09b825ff22e927520e1e91181929434
Shridhar Sukhani <shridharsukhani@outlook.com>
2019-01-28 11:27:16 -0600

    Implement Authentication with Firebase

--- Clonestagram/Controllers/LoginViewController.swift
+++ Clonestagram/Controllers/LoginViewController.swift
@@ -7,7 +7,10 @@
 //
 
 import UIKit
+import FirebaseUI
+import FirebaseAuth
 
+typealias FIRUser = FirebaseAuth.User
 class LoginViewController: ViewController {
     @IBOutlet weak var registerOrLoginButton: UIButton!
     
@@ -23,5 +26,26 @@ class LoginViewController: ViewController {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // TODO: Finish this
+    }
+    
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        guard let authUI = FUIAuth.defaultAuthUI() else {
+            return
+        }
+        
+        authUI.delegate = self
+        
+        let authViewController = authUI.authViewController()
+        present(authViewController, animated: true)
+    }
+}
+
+extension LoginViewController: FUIAuthDelegate {
+    func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
+        if let error = error {
+            assertionFailure("Error signing in: \(error.localizedDescription)")
+            return
+        }
+        print("Handle User Sign Up/Log In")
     }
 }
